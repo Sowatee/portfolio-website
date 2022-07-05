7f7a0e31ad7be661fee225a393a433dd326bdf01
Sowatee <jamesprescott9000@gmail.com>
2022-07-04 19:23:32 -0500

    data file containing all projects for purposes of loop rendering

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    name: "Alchemist Field Guide",
+    description: "I created the alchemist field guide to make it easy to search and filter through the almost 200 ingredients of Skyrim's alchemical system.",
+    image: "https://images.uesp.net/8/8e/SR-cover-Skyrim_Anniversary_Edition.jpg",
+    github: "https://github.com/Sowatee/skyrim-alchemist-field-guide",
+    link: "https://alchemistfieldguide.netlify.app/"
+  },
+  {
+    name: "Expenses Tracker",
+    description: "I created this website to help with creating a budget and tracking my expenses. It utilizes Chart.js for data visualization.",
+    image: "https://i.imgur.com/iJPN8JP.jpg",
+    github: "",
+    link: ""
+  },
+  {
+    name: "The Medieval Emporium",
+    description: "An e-commerce store for a fictional maker of medieval weapons and armor made with React Router and Redux.",
+    image: "https://i.imgur.com/oBZYGDk.jpg",
+    github: "",
+    link: ""
+  },
+]
